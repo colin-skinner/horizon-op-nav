961c6d4d366957a8023cb71868a062fa041f4187
Colin Skinner <colinaskinner@gmail.com>
2026-03-08 11:14:27 -0700

    Sunday fixes (#3)
    * Reorganization
    
    * More

--- PlanetaryData.py
+++ PlanetaryData.py
@@ -1,33 +1,29 @@
 # import numpy as np
 from Constants import G
 
-print("PlanetaryData:Make sure to update position of bodies with position at specific time")
-print("e.g. Earth.position = [1,2,3]")
-class Body:
-    def __init__(self, name: str, mass: float, mu: float, radius: float, position: float = None):
-        self.name = name
-        self.mass = mass
-        self.mu = mu
-        self.radius = radius
-        self.position = position
+print("PlanetaryData: Make sure to update position of bodies with position at specific time")
+print('e.g. `Earth["position"] = [1.0, 2.0, 3.0]  # km`')
 
-Sun = Body(
-    name = "Sun",
-    mass = 1.989e30, # kg
-    mu = 1.989e30*G, # km3/s2
-    radius = 695510.0, # km
-)
+Sun = {
+    "name": "Sun",
+    "mass": 1.989e30,       # kg
+    "mu": 1.989e30 * G,     # km^3/s^2
+    "radius": 695510.0,     # km
+    "position": None,
+}
 
-Earth = Body(
-    name = "Sun",
-    mass = 5.972e24, # kg
-    mu = 5.972e24*G, # km3/s2
-    radius = 6378.0, # km
-)
+Earth = {
+    "name": "Earth",
+    "mass": 5.972e24,       # kg
+    "mu": 5.972e24 * G,     # km^3/s^2
+    "radius": 6378.0,       # km
+    "position": None,
+}
 
-Luna = Body(
-    name = "THE Moon",
-    mass = 7.34767309e22, # kg
-    mu = 7.34767309e22*G, # km3/s2
-    radius = 1737.1, # km
-)
+Luna = {
+    "name": "Moon",
+    "mass": 7.34767309e22,  # kg
+    "mu": 7.34767309e22 * G,# km^3/s^2
+    "radius": 1737.1,       # km
+    "position": None,
+}
